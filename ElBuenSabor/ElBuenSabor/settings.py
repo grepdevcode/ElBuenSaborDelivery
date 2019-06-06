09843fae9fc46eceff1610b5adfdc7bf4b19d0c3
Martin0001 <navarromartin762@yahoo.com.ar>
2019-06-06 02:40:32 -0300

    Conection to data base

--- ElBuenSabor/ElBuenSabor/settings.py
+++ ElBuenSabor/ElBuenSabor/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ElBuenSabor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'proylab',
+        'USER':'postgres',
+        'PASSWORD': '',#agregar contraseña correspondiente
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
